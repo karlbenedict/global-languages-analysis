892b8f1de3523e470e8ab64084a2422aacc4c356
Karl Benedict <kbene@unm.edu>
2022-11-21 04:33:46 +0000

    setup include file

---
+++ scripts/00_include.R
@@ -0,0 +1,60 @@
+library(rgrass)
+library(sp)
+library(lubridate)
+library(stringi)
+library(gdalUtilities)
+library(rgdal)
+library(raster)
+library(tidyverse)
+library(readr)
+library(forcats)
+library(readxl)
+
+# legacy - delete if not needed later
+# use_sp() # https://gis.stackexchange.com/questions/341451/rgrass7-no-stars-import-yet
+
+# setup environment variables
+rm(list = ls())
+Sys.setenv(LC_ALL = "en_US.UTF-8",
+           LANG = "en_US.UTF-8",
+           CPL_ZIP_ENCODING = "UTF-8")
+
+
+
+# define the local path to the project root folder ####################################################################
+projectRoot <- "/Volumes/big-red/analyses/globalTemp/"
+gisBase <- "/Applications/GRASS-7.8.app/Contents/Resources"
+
+varNames <- c("GISBASE",
+              "GISDBASE",
+              "GISRC",
+              "GIS_LOCK",
+              "GRASS_PAGER",
+              "LD_LIBRARY_PATH",
+              "LOCATION_NAME",
+              "MAPSET",
+              "PROJ_LIB",
+              "PYTHONPATH"
+)
+
+tempDatedFolder <- function(basepath, gisDbase, location, mapset) {
+  fixedTime <- now()
+  tempdir <- paste(basepath, "temp/", sep = "")
+  dir.create(tempdir)
+  temppath <- paste(tempdir, "grassTemp_", paste(date(fixedTime), "T", hour(fixedTime), ":", minute(fixedTime), ":", second(fixedTime), "_", stri_rand_strings(1, 5), sep = ""), sep = "")
+  dir.create(temppath)
+  #fileConn <-  file(paste(temppath, "/.grassrc7", sep = ""))
+  #writeLines(paste("GISDBASE: ", gisDbase, sep = ""), fileConn)
+  #writeLines(paste("LOCATION_NAME: ", location, sep = ""), fileConn)
+  #writeLines(paste("MAPSET: ", mapset, sep = ""), fileConn)
+  #close(fileConn)
+  print(paste("Temp home directory: ", temppath, sep = ""))
+  temppath
+}
+
+clearVars <- function(varList) {
+  for (envVar in varList) {
+    Sys.unsetenv(envVar)
+  }
+}
+
