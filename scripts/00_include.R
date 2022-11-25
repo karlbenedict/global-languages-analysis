1891b4e921edea4385a4d64b103c6132212ad368
Karl Benedict <kbene@unm.edu>
2022-11-25 22:39:25 +0000

    add ps generation function for quick figure generation

--- scripts/00_include.R
+++ scripts/00_include.R
@@ -39,14 +39,135 @@ varNames <- c("GISBASE",
               "PYTHONPATH"
 )
 
-tempDatedFolder <- function(basepath, gisDbase, location, mapset) {
+tempDatedFolder <- function() {
   fixedTime <- now()
-  tempdir <- paste(basepath,"/", "temp/", sep = "")
-  dir.create(tempdir)
-  temppath <- paste(tempdir, "grassTemp_", paste(date(fixedTime), "T", hour(fixedTime), ":", minute(fixedTime), ":", second(fixedTime), "_", stri_rand_strings(1, 5), sep = ""), sep = "")
+  tempdir <- paste(projectRoot,"/", "temp/", sep = "")
+  temppath <- paste(tempdir, "working_", paste(date(fixedTime), "T", hour(fixedTime), ":", minute(fixedTime), ":", second(fixedTime), "_", stri_rand_strings(1, 5), sep = ""), sep = "")
   dir.create(temppath)
-  print(paste("Temp home directory: ", temppath, sep = ""))
   temppath
+}
+
+ps_preview <- function(raster="", 
+                       vPoints=c(), 
+                       vPoints_colors=c(),
+                       vLines=c(), 
+                       vLines_colors=c(),
+                       vAreas=c(), 
+                       vAreas_colors=c(), 
+                       vAreas_fcolors=c(), 
+                       geogrid=TRUE, 
+                       scalebar=FALSE, 
+                       mapinfo=TRUE,
+                       title="Default map preview"){
+  # this function must be executed within a defined GRASS environment context
+  workingDir <- tempDatedFolder()
+  commandFile <- paste(workingDir, "commands.txt", sep="/")
+  headerFile <- paste(workingDir, "header.txt", sep="/")
+  outputFile <- paste(workingDir, "output.ps", sep="/")
+  print(paste("The output will be generated in the following temp folder:", workingDir, sep=" "))
+  print(paste("The command file will be:", commandFile, sep=" "))
+  print(paste("The header file will be:", headerFile, sep=" "))
+
+  cat("%_", fill=TRUE, file=headerFile, append=TRUE)
+  cat(title, fill=TRUE, file=headerFile, append=TRUE)
+  cat("Basemap: %c", fill=TRUE, file=headerFile, append=TRUE)
+  
+  vectorLayers <- c()
+    
+  if (raster == "") {
+    print("No raster will be added")
+  } else {
+    print(paste("The basemap raster is:", raster, sep=" "))
+    cat(c(paste("raster", raster, sep=" ")), fill=TRUE, file=commandFile, append=TRUE)
+  }
+  if (length(vPoints)>0) {
+    print("The points(s) and associated colors are:")
+    for (i in 1:length(vPoints)){
+      print(paste("     ",vPoints[i], vPoints_colors[i]))
+      vectorLayers <- c(vectorLayers,vPoints[i])
+      cat(paste("vpoints",vPoints[i],sep=" "), fill=TRUE, file=commandFile, append=TRUE)
+      cat(paste("    color",vPoints_colors[i], sep=" "), fill=TRUE, file=commandFile, append=TRUE)
+      cat("    size .25" , fill=TRUE, file=commandFile, append=TRUE)
+      cat("end", fill=TRUE, file=commandFile, append=TRUE)
+    }
+  } else {
+    print("No vector points will be added")
+  }
+  if (length(vAreas)>0) {
+    print("The vector areas(s) and associated line and fill colors are:")
+    for (i in 1:length(vAreas)){
+      print(paste("     ",vAreas[i], vAreas_colors[i], vAreas_fcolors[i]))
+      vectorLayers <- c(vectorLayers,vAreas[i])
+      cat(paste("vareas",vAreas[i],sep=" "), fill=TRUE, file=commandFile, append=TRUE) 
+      cat(paste("    color",vAreas_colors[i],sep=" "), fill=TRUE, file=commandFile, append=TRUE)
+      cat(paste("    fcolor",vAreas_fcolors[i],sep=" "), fill=TRUE, file=commandFile, append=TRUE)
+      cat(paste("    width .1",vAreas_fcolors[i],sep=" "), fill=TRUE, file=commandFile, append=TRUE)
+      cat("end", fill=TRUE, file=commandFile, append=TRUE)
+    } 
+  } else {
+    print("No vector areas will be added")
+  }
+  if (length(vLines)>0) {
+    print("The vector(s) and associated colors are:")
+    for (i in 1:length(vLines)){
+      print(paste("     ",vLines[i], vLines_colors[i]))
+      vectorLayers <- c(vectorLayers,vAreas[i])
+      cat(paste("vlines",vLines[i],sep=" "), fill=TRUE, file=commandFile, append=TRUE)
+      cat(paste("    color",vLines_colors[i], sep=" "), fill=TRUE, file=commandFile, append=TRUE)
+      cat("    width .1" , fill=TRUE, file=commandFile, append=TRUE)
+      cat("end", fill=TRUE, file=commandFile, append=TRUE)
+    }
+  } else {
+    print("No vector lines will be added")
+  }
+  if (geogrid) {
+    cat("geogrid 10 d", fill=TRUE, file=commandFile, append=TRUE)
+    cat("    color gray", fill=TRUE, file=commandFile, append=TRUE)
+    cat("    width .1" , fill=TRUE, file=commandFile, append=TRUE)
+    cat("end", fill=TRUE, file=commandFile, append=TRUE)
+  }
+  if (scalebar) {
+    cat("scalebar f", fill=TRUE, file=commandFile, append=TRUE)
+    cat("    length 90", fill=TRUE, file=commandFile, append=TRUE)
+    cat("    units auto" , fill=TRUE, file=commandFile, append=TRUE)
+    cat("    height .25" , fill=TRUE, file=commandFile, append=TRUE)
+    cat("    segment 5" , fill=TRUE, file=commandFile, append=TRUE)
+    cat("end", fill=TRUE, file=commandFile, append=TRUE)
+  }
+  if (mapinfo) {
+    cat("mapinfo", fill=TRUE, file=commandFile, append=TRUE)
+    cat("    color black", fill=TRUE, file=commandFile, append=TRUE)
+    cat("    border black" , fill=TRUE, file=commandFile, append=TRUE)
+    cat("    fontsize 4" , fill=TRUE, file=commandFile, append=TRUE)
+    cat("    where 7.75 0.375" , fill=TRUE, file=commandFile, append=TRUE)
+    cat("end", fill=TRUE, file=commandFile, append=TRUE)
+  }
+
+  cat("Vector layers: ", fill=FALSE, file=headerFile, append=TRUE)
+  cat(paste(vectorLayers, collapse=", "), fill=TRUE, file=headerFile, append=TRUE)
+  cat("Produced by: Karl Benedict, %d", fill=TRUE, file=headerFile, append=TRUE)
+  cat("%_", fill=TRUE, file=headerFile, append=TRUE)
+
+  cat("header" , fill=TRUE, file=commandFile, append=TRUE)
+  cat(paste("    file ", headerFile, sep=""), fill=TRUE, file=commandFile, append=TRUE)
+  cat("    fontsize 5" , fill=TRUE, file=commandFile, append=TRUE)
+  cat("    color black" , fill=TRUE, file=commandFile, append=TRUE)
+  cat("end", fill=TRUE, file=commandFile, append=TRUE)
+  
+  cat("paper" , fill=TRUE, file=commandFile, append=TRUE)
+  cat("    width 9" , fill=TRUE, file=commandFile, append=TRUE)
+  cat("    height 6" , fill=TRUE, file=commandFile, append=TRUE)
+  cat("    left .25" , fill=TRUE, file=commandFile, append=TRUE)
+  cat("    right .25" , fill=TRUE, file=commandFile, append=TRUE)
+  cat("    top .25" , fill=TRUE, file=commandFile, append=TRUE)
+  cat("    bottom .25" , fill=TRUE, file=commandFile, append=TRUE)
+  cat("end", fill=TRUE, file=commandFile, append=TRUE)
+  execGRASS("ps.map",
+            flags=c("verbose"),
+            parameters=list(input=commandFile,
+                            output=outputFile),
+            echoCmd=TRUE
+  )
 }
 
 clearVars <- function(varList) {
@@ -55,16 +176,20 @@ clearVars <- function(varList) {
   }
 }
 
-GRASSwrapper <- function(location, mapset, command, params, flags) {
-  initGRASS(
-    gisBase = gisBase,
-    home = tempdir(),  #tempDatedFolder(projectRoot),
-    gisDbase = gisDBase,
-    location = location,
-    mapset = mapset,
-    override = TRUE
-  )
-  doGRASS(command, flags=flags, parameters=params, echoCmd=TRUE)
-  stringexecGRASS("g.region -p")
+# retrieve the zip file specified in the fileURL parameter, put
+# it into the temp directory, and unzip it. Return the namem of
+# the uncompressed directory containing the zip archive contents
+getSourceZip <- function(fileURL){
+  downloadDirectory <- paste(getwd(), "/temp", sep="")
+  # download
+  commandString <- paste("wget -nc --directory-prefix=",downloadDirectory," ",fileURL, sep="" )
+  print(commandString)
+  system(commandString)
+  # uncompress
+  zipFile <- paste(downloadDirectory,basename(fileURL),sep="/")
+  unzippedDirectory <- file_path_sans_ext(zipFile)
+  commandString <- paste("unzip -u",zipFile, "-d", downloadDirectory, sep=" " )
+  print(commandString)
+  system(commandString)
+  return(unzippedDirectory)
 }
-
