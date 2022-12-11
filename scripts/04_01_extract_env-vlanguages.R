f30fd11491fa04de13f822f02549d598e595f100
Karl Benedict <kbene@unm.edu>
2022-12-11 17:11:11 +0000

    analysis and visualization

---
+++ scripts/04_01_extract_env-vlanguages.R
@@ -0,0 +1,155 @@
+# 04_01_env-vlanguages.R
+# extract environmental statistics into language voronoi polygons
+
+# execute the shared setup file
+source("scripts/00_include.R")
+
+polygonsLayer <- "languages_voronoi_clipped"
+fixedTime <- now()
+outputDataFile <- paste(projectRoot,"/output/data/v-languages.csv", sep="")
+
+location <- "4326"
+mapset <- "PERMANENT"
+initGRASS(
+  gisBase = gisBase,
+  home = projectRoot,
+  gisDbase = gisDBase,
+  location = location,
+  mapset = mapset,
+  override = TRUE
+)
+
+
+##### Elevation stats #########################################################
+rasterLayer <- "ACE2_elevation"
+execGRASS("g.region", 
+          flags=c("verbose","p"), 
+          parameters=list(res="00:00:30"), 
+          echoCmd=TRUE)
+execGRASS("v.rast.stats",
+          flags=c("verbose","c","d"),
+          parameters=list(map = polygonsLayer,
+                          raster = rasterLayer,
+                          column_prefix = "v_elev_m_"),
+          echoCmd=TRUE)
+execGRASS("v.info",
+          flags=c("c"),
+          parameters=list(map = polygonsLayer),
+          echoCmd=TRUE)
+
+##### Specific Hummidity stats ################################################
+rasterLayer <- "cdas_qa_30_yr_avg"
+execGRASS("g.region", 
+          flags=c("verbose","p"), 
+          parameters=list(res="00:00:30"), 
+          echoCmd=TRUE)
+execGRASS("v.rast.stats",
+          flags=c("verbose","c","d"),
+          parameters=list(map = polygonsLayer,
+                          raster = rasterLayer,
+                          column_prefix = "v_qa_unitless_"),
+          echoCmd=TRUE)
+execGRASS("v.info",
+          flags=c("c"),
+          parameters=list(map = polygonsLayer),
+          echoCmd=TRUE)
+
+
+##### Biomass stats ###########################################################
+rasterLayer <- "Aboveground_Live_Woody_Biomass_Density"
+execGRASS("g.region", 
+          flags=c("verbose","p"), 
+          parameters=list(res="00:00:30"), 
+          echoCmd=TRUE)
+execGRASS("v.rast.stats",
+          flags=c("verbose","c","d"),
+          parameters=list(map = polygonsLayer,
+                          raster = rasterLayer,
+                          column_prefix = "v_biomass_MgHa_"),
+          echoCmd=TRUE)
+execGRASS("v.info",
+          flags=c("c"),
+          parameters=list(map = polygonsLayer),
+          echoCmd=TRUE)
+
+
+##### Landcover stats ###########################################################
+rasterLayer <- "glc2000_v1_1_reclass"
+execGRASS("g.region", 
+          flags=c("verbose","p"), 
+          parameters=list(res="00:00:30"), 
+          echoCmd=TRUE)
+# all cells within a language polygon
+execGRASS("v.rast.stats",
+          flags=c("verbose","c","d"),
+          parameters=list(map = polygonsLayer,
+                          raster = rasterLayer,
+                          column_prefix = "v_lc_all_ct_",
+                          method = c("number","null_cells")),
+          echoCmd=TRUE)
+# generate and tally logical raster content for each land cover type
+lcClasses <- list(c("lc_tall","1"),
+                  c("lc_med","2"),
+                  c("lc_short","3"),
+                  c("lc_water","10"),
+                  c("lc_snow","11"))
+for (lcClass in lcClasses) {
+  execGRASS("r.mapcalc",
+            flags=c("overwrite","verbose"),
+            parameters=list(expression = paste(lcClass[1]," = ", rasterLayer, " == ", lcClass[2], sep="")),
+            echoCmd=TRUE
+  )
+  execGRASS("v.rast.stats",
+            flags=c("verbose","c","d"),
+            parameters=list(map = polygonsLayer,
+                            raster = lcClass[1],
+                            column_prefix = paste("v_",lcClass[1],"_ct_",sep=""),
+                            method = c("number","null_cells","sum")),
+            echoCmd=TRUE
+  )
+  
+}
+execGRASS("v.info",
+          flags=c("c"),
+          parameters=list(map = polygonsLayer),
+          echoCmd=TRUE)
+
+
+##### Global climate data stats ###############################################
+# calculation of average annual statistic for all station-years within each 
+# polygon
+statColumns <- list(c("TAVG","v_tavg_dC_"),
+                    c("TMAX","v_tmax_dC_"),
+                    c("TMIN","v_tmin_dC_"),
+                    c("PRCP","v_prcp_mm_"))
+pointsLayer <- "gsoy"
+# make a copy of the layer to cast column values to numeric 
+
+for (pointColumn in statColumns){
+  execGRASS("v.vect.stats",
+            flags=c("verbose"),
+            parameters=list(points = pointsLayer,
+                             areas = polygonsLayer,
+                             points_where = paste(pointColumn[1],"IS NOT NULL", sep=" "),
+                             method = "average",
+                             points_column = pointColumn[1],
+                             count_column = paste(pointColumn[2],"_ct", sep = ""),
+                             stats_column = paste(pointColumn[2],"_avg", sep = "")),
+            echoCmd=TRUE
+  )
+  
+}
+execGRASS("v.info",
+          flags=c("c"),
+          parameters=list(map = pointsLayer),
+          echoCmd=TRUE)
+execGRASS("v.info",
+          flags=c("c"),
+          parameters=list(map = polygonsLayer),
+          echoCmd=TRUE)
+
+
+
+##### Export extracted data to CSV for further analysis #######################
+outData <- distinct(as.data.frame(read_VECT(polygonsLayer)))
+write.csv(outData, file=outputDataFile)
---
+++ scripts/05_01_analysis_distributions_corr.R
@@ -0,0 +1,201 @@
+# 05_01_analysis_distributions_corr.R
+# Analysis of distributions of language and environmental variables
+
+# execute the shared setup file
+source("scripts/00_include.R")
+
+library(ggplot2)
+library(corrplot)
+
+languageDataFile <- paste(projectRoot,"/output/data/v-languages_2022-12-02T16:28.csv",sep="")
+gsoyDataFile <- paste(projectRoot,"/output/data/gsoy.csv",sep="")
+outputDir <- paste(projectRoot,"/output/images",sep="")
+
+languageData <- read_csv(languageDataFile)
+gsoyData <- read_csv(gsoyDataFile)
+
+# read data from language vector file
+location <- "4326"
+mapset <- "PERMANENT"
+initGRASS(
+  gisBase = gisBase,
+  home = projectRoot,
+  gisDbase = gisDBase,
+  location = location,
+  mapset = mapset,
+  override = TRUE
+)
+
+##### Language Characteristic Distributions ###################################
+languageVariablesNum <- c('Vowindex',
+                       'Onset',
+                       'Coda',
+                       'VQ',
+                       'VTot',
+                       'CTotal',
+                       'Obstr',
+                       'ObsPct',
+                       'SegTot',
+                       'CplusVQ',
+                       'ObsPct',
+                       'ToneOrdinal',
+                       'OnsCoda',
+                       'ConsHeavy',
+                       'CHeavyObstr',
+                       'CHeavyLog',
+                       'LogSpkrs',
+                       'xEjectives',
+                       'xImplosives',
+                       'xGlotRes'
+                       )
+languageVariablesChar <- c('ToneCat',
+                          'FrRndV',
+                          'GlotC_',
+                          'Ejectives',
+                          'Implosives',
+                          'GlotRes',
+                          'VelarNas',
+                          'NVPattern',
+                          'PNC_s',
+                          'VLength'
+)
+
+# histograms for numeric variables
+for (langVar in languageVariablesNum) {
+  # for each language variable generate a distribution plot
+  langVar <- sym(langVar)
+  print(langVar)
+  minVal <- min(languageData[langVar])
+  maxVal <- max(languageData[langVar])
+  range <- maxVal-minVal
+  binWidth <- range/15
+  cat(minVal,"-",maxVal,sep="",fill=TRUE)
+  currPlot <- ggplot(languageData, aes(x=!!langVar)) + 
+    geom_histogram(binwidth = binWidth) +
+    theme_bw()
+  outFile <- paste(langVar,"_hist.png", sep="")
+  cat(outFile,fill=TRUE)
+  ggsave(outFile,
+         plot=currPlot,
+         path=outputDir,
+         width=9,
+         height=6,
+         units="in")
+}
+
+# column charts for character variables
+for (langVar in languageVariablesChar) {
+  # for each language variable generate a distribution plot
+  langVar <- sym(langVar)
+  print(langVar)
+  currPlot <- ggplot(languageData, aes(x=!!langVar)) + 
+    geom_bar() +
+    theme_bw()
+  outFile <- paste(langVar,"_col.png", sep="")
+  cat(outFile,fill=TRUE)
+  ggsave(outFile,
+         plot=currPlot,
+         path=outputDir,
+         width=9,
+         height=6,
+         units="in")
+}
+
+##### Environmental variables #################################################
+envVariables <- c(
+  'v_elev_m__minimum',
+  'v_elev_m__maximum',
+  'v_elev_m__average',
+  'v_elev_m__median',
+  'v_qa_unitless__minimum',
+  'v_qa_unitless__maximum',
+  'v_qa_unitless__average',
+  'v_qa_unitless__median',
+  'v_biomass_MgHa__minimum',
+  'v_biomass_MgHa__maximum',
+  'v_biomass_MgHa__average',
+  'v_biomass_MgHa__median',
+  'v_lc_all_ct__number',
+  'v_lc_tall_ct__sum',
+  'v_lc_med_ct__sum',
+  'v_lc_short_ct__sum',
+  'v_lc_water_ct__sum',
+  'v_lc_snow_ct__sum',
+  'v_tavg_dC__ct',
+  'v_tavg_dC__avg',
+  'v_tmax_dC__ct',
+  'v_tmax_dC__avg',
+  'v_tmin_dC__ct',
+  'v_tmin_dC__avg',
+  'v_prcp_mm__ct',
+  'v_prcp_mm__avg'
+)
+for (envVar in envVariables) {
+  # for each language variable generate a distribution plot
+  cat("",fill=TRUE)
+  envVar <- sym(envVar)
+  print(envVar)
+  plotData <- languageData %>% 
+    filter(!is.na(!!envVar))
+  cat(nrow(plotData),fill=TRUE)
+  minVal <- min(plotData[envVar])
+  maxVal <- max(plotData[envVar])
+  range <- maxVal-minVal
+  binWidth <- range/15
+  cat(minVal,"-",maxVal,sep="",fill=TRUE)
+  currPlot <- ggplot(plotData, aes(x=!!envVar)) + 
+    geom_histogram(binwidth = binWidth) +
+    labs(x = paste(envVar," (n=",nrow(plotData),") ", sep="")) +
+    theme_bw()
+  outFile <- paste(envVar,"_hist.png", sep="")
+  cat(outFile,fill=TRUE)
+  ggsave(outFile,
+         plot=currPlot,
+         path=outputDir,
+         width=9,
+         height=6,
+         units="in")
+}
+
+##### Environmental variables correlogram #####################################
+envVariablesCorr <- c(
+  'v_elev_m__median',
+  'v_qa_unitless__median',
+  'v_biomass_MgHa__median',
+  'v_lc_tall_ct__sum',
+  'v_lc_med_ct__sum',
+  'v_lc_short_ct__sum',
+  'v_lc_water_ct__sum',
+  'v_lc_snow_ct__sum',
+  'v_tavg_dC__avg',
+  'v_tmax_dC__avg',
+  'v_tmin_dC__avg',
+  'v_prcp_mm__avg'
+)
+corrPlotData <- languageData %>% 
+  select(all_of(envVariablesCorr)) %>% 
+  filter_at(vars(paste(envVariablesCorr,sep=",")),all_vars(!is.na(.)))
+corrM <- cor(corrPlotData)
+png(height=2700, width=2700, file=paste(outputDir,"/env_corr.png",sep=""),type="cairo")
+corrplot(corrM,
+         method="circle",
+         tl.cex=3,
+         tl.col="black",
+         cl.cex=2,
+         type="upper")
+dev.off()
+
+##### Language and environmental variables ####################################
+corrPlotData <- languageData %>% 
+  select(all_of(c(envVariablesCorr,languageVariablesNum))) %>% 
+  filter_at(vars(paste(c(envVariablesCorr,languageVariablesNum),sep=",")),all_vars(!is.na(.)))
+corrM <- cor(corrPlotData)
+png(height=5400, width=5400, file=paste(outputDir,"/env_lang_corr.png",sep=""),type="cairo")
+corrplot(corrM,
+         method="circle",
+         tl.cex=5,
+         tl.col="black",
+         cl.cex=4,
+         type="upper")
+dev.off()
+
